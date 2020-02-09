257f77b62257cf123ff449f23a6fb3930c192184
Osvaldo <osvaldofabricio11@gmail.com>
2020-02-08 19:02:49 -0500

    Updating Files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ExpressJS-ReactJs-Heroku-Template
+
+This is a template that uses Express JS as backend and React JS as the front-end. The app implements the necessary files to be used on the Heroku Platform.
---
+++ app.js
@@ -0,0 +1,22 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+app.use(express.static(path.join(__dirname, "client/build")));
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+app.listen(port);
