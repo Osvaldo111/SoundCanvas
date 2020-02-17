450b46d8543e5cb0796dd62a6dadc638ba65dfbb
Osvaldo <osvaldofabricio11@gmail.com>
2020-02-17 01:34:05 -0500

    Canvas update

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,13 +1,97 @@
 import React from "react";
 import "../style/sideBar.css";
+import { connect } from "react-redux";
+import { displayMobileBttn } from "../actions";
+
 /**
  * author: Osvaldo Carrillo
  * Date: 02/08/2020
  * This class is designed to be the sidebar or the edition
  * bar in the application.
  */
-export default class SideBar extends React.Component {
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+
+    this.state = {
+      contaninerWidth: false,
+      widthFirstSide: null,
+      widthSecondSide: null,
+      closeMobile: false,
+      displayOpacity: false
+    };
+  }
+
+  closeMobileBar = () => {
+    this.setState({
+      contaninerWidth: false,
+      widthFirstSide: "0px",
+      widthSecondSide: "0px",
+      displayOpacity: "false"
+    });
+
+    // Reducer to display the menu bttn in mobile
+    this.props.displayMobileBttn(true);
+  };
+
+  displayMobileSideBar = () => {
+    this.setState({
+      contaninerWidth: true,
+      widthFirstSide: "60px",
+      widthSecondSide: "190px",
+      displayOpacity: "true"
+    });
+  };
+
+  componentDidUpdate(prevProps) {
+    //Check when the mobile bttn is press
+    const currentBttnPress = this.props.canvasProps.checkBttnPress;
+    const prevBttnPress = prevProps.canvasProps.checkBttnPress;
+    if (currentBttnPress != prevBttnPress) {
+      console.log(currentBttnPress, "   ", prevBttnPress);
+      this.displayMobileSideBar();
+    }
+  }
+
   render() {
-    return <div className="sideBarContainer"></div>;
+    const { closeMobile } = this.state;
+    const { displayOpacity } = this.state;
+    const { contaninerWidth } = this.state;
+    console.log(closeMobile);
+    return (
+      <div
+        className="sideBarContainer"
+        style={{ width: contaninerWidth ? "100%" : "" }}
+      >
+        <div
+          className="sideBarFirst"
+          style={{ width: this.state.widthFirstSide }}
+        >
+          <p>Backgro</p>
+          <p>Backgr</p>
+        </div>
+        <div
+          className="sideBarSecond"
+          style={{ width: this.state.widthSecondSide }}
+        >
+          <div>Backgrounddd</div>
+        </div>
+        <div
+          className="closeSideBar"
+          onClick={this.closeMobileBar}
+          style={{ flexGrow: displayOpacity ? "1" : "0" }}
+          onClick={this.closeMobileBar}
+        ></div>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return { canvasProps: state.canvas };
+}
+const mapDispatchToProps = {
+  displayMobileBttn
+};
+export default connect(mapStateToProps, mapDispatchToProps)(SideBar);
---
+++ client/src/reducers/canvas.js
@@ -0,0 +1,22 @@
+import { CHANGE_CANVAS_WIDTH } from "../constants/ActionTypes";
+import { CANVAS_MOBILE_BTTN } from "../constants/ActionTypes";
+import { CANVAS_REDISPLAY_MOBILE_BTTN } from "../constants/ActionTypes";
+
+const init = {
+  width: 0,
+  checkBttnPress: false,
+  displayMobileBttn: false
+};
+
+export default function(state = init, action) {
+  switch (action.type) {
+    case CHANGE_CANVAS_WIDTH:
+      return { width: action.width };
+    case CANVAS_MOBILE_BTTN:
+      return { check: action.checkBttnPress };
+    case CANVAS_REDISPLAY_MOBILE_BTTN:
+      return { displayMobileBttn: action.displayMobileBttn };
+    default:
+      return state;
+  }
+}
