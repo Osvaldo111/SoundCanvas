b6a3da1678b2b9e5dca865c4dcec731d0995cd00
Osvaldo <osvaldofabricio11@gmail.com>
2020-03-16 03:50:24 -0500

    Settingup postgresql from Heroku

--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,15 @@
 const { Client } = require("pg");
 
+// const client = new Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "soundcanvas",
+//   password: "123",
+//   port: "5432"
+// });
+
 const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "soundcanvas",
-  password: "123",
-  port: "5432"
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 module.exports = client;
